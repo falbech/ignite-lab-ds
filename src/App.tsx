6c53b9fedee6130d103859b105e1ae1e868c8629
Felipe Martinez Albeche <felipealbeche@hotmail.com>
2022-10-25 17:19:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl  text-violet-500">Hello world!</h1>
+      <button className="bg-cyan-500 hover:bg-cyan-300"></button>
+    </>
+  )
+}
